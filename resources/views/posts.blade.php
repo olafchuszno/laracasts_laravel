c42ea2b94777bcae2014a9e3e4a6d9cc5775a994
Olaf <olaf.chuszno@gmail.com>
2023-08-01 14:53:22 +0200

    After 14th lesson

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,20 +8,20 @@
 
 <body>
 
-    <?php foreach ($posts as $post) : ?>
-        <article>
+    @foreach ($posts as $post) :
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
 
             <h1>
-                <a href="posts/<?= $post->slug ?>">
-                    <?= $post->title; ?>   
+                <a href="posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a> 
             </h1>
 
             <div>
-                <?= $post->excerpt; ?>
+                {{ $post->excerpt }}
             </div>
 
         </article> 
-    <?php endforeach; ?>
+    @endforeach;
 
 </body>
