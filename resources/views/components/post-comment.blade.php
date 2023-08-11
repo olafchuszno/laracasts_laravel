17dd7dd50dc1100dc4295d22d0f81154f6dc8be8
Olaf <olaf.chuszno@gmail.com>
2023-08-11 14:14:29 +0200

    After 52nd lesson

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,24 @@
+<article class="flex space-x-4 bg-gray-100 border border-gray-200 rounded-xl p-6">
+    <div style="flex-shrink: 0">
+        <img src="https://i.pravatar.cc/60" 
+            alt="user's avatar"
+            width="60"
+            height="60"
+            class="rounded-xl"
+        >
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+        </p>
+    </div>
+</article>
