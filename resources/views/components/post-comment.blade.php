2eaf9a991595bae5285f6796ecefe4654bfc4fc9
Olaf <olaf.chuszno@gmail.com>
2023-08-13 15:59:11 +0200

    After 55th lesson

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,26 +1,28 @@
 @props(['comment'])
 
-<article class="flex space-x-4 bg-gray-100 border border-gray-200 rounded-xl p-6">
-    <div style="flex-shrink: 0">
-        <img src="https://i.pravatar.cc/60?id={{ $comment->id }}" 
-            alt="user's avatar"
-            width="60"
-            height="60"
-            class="rounded-xl"
-        >
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div style="flex-shrink: 0">
+            <img src="https://i.pravatar.cc/60?id={{ $comment->id }}" 
+                alt="user's avatar"
+                width="60"
+                height="60"
+                class="rounded-xl"
+            >
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->name }}</h3>
-            <p class="text-xs">
-                Posted
-                <time>{{ $comment->created_at->diffForHumans() }}</time>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->name }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,13 +52,48 @@
                     </div>
                 </div>
 
-                @if (count($post->comments))
+
                     <section class="col-span-8 col-start-5 mt-10 space-y-6">
+
+                        <x-panel>
+
+                            <form method="POST" action="#">
+                                @csrf
+
+                                <header class="flex items-center">
+                                    <img src="https://i.pravatar.cc/60?id={{ auth()->id() }}" 
+                                        alt="user's avatar"
+                                        width="40"
+                                        height="40"
+                                        class="rounded-full"
+                                    >
+
+                                    <h2 class="ml-4">Want to participate?</h2>
+                                </header>
+
+                                <div class="mt-6">
+                                    <textarea name="body"
+                                    class="w-full text-sm focus:outline-none focus:ring" 
+                                    id="body" 
+                                    rows="5" 
+                                    placeholder="Quick, think of something to say!"
+                                    ></textarea>
+                                </div>
+
+                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                    <button type="submit"
+                                            class="bg-blue-500 text-white uppercase font-semibold text-s py-2 px-10 rounded-3xl hover:bg-blue-600"
+                                    >Post</button>
+                                </div>
+
+                            </form>
+
+                        </x-panel>
+
                         @foreach ($post->comments as $comment)
                             <x-post-comment :comment="$comment"/>
                         @endforeach
                     </section>
-                @endif
 
             </article>
         </main>
