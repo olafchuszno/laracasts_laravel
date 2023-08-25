7288e9479576b3b6ab7ea477ac60bc1928ef89ad
Olaf <olaf.chuszno@gmail.com>
2023-08-25 15:17:16 +0200

    Implement bookmark's factory

--- database/factories/BookmarkFactory.php
+++ database/factories/BookmarkFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,8 @@ class BookmarkFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'post_id' => Post::factory()
         ];
     }
 }
