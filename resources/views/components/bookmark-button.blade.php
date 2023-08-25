20164eaeae5ae11f4e4b76971cb77a92d24035f5
Olaf <olaf.chuszno@gmail.com>
2023-08-25 18:58:20 +0200

    Style the Bookmark blade component

--- resources/views/components/bookmark-button.blade.php
+++ resources/views/components/bookmark-button.blade.php
@@ -5,8 +5,11 @@
         @csrf
         @method('store')
         
-        <button type="submit">
-            Add to bookmarked
+        <button type="submit"
+            class="px-3 py-1 border border-red-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+            style="font-size: 10px"
+        >
+            Add Post to bookmarked
         </button>
 
     </form>
