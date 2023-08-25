b126af7c8ccff407069135b84bdeaae8f2d01f3f
Olaf <olaf.chuszno@gmail.com>
2023-08-25 12:52:50 +0200

    Change the bookmark's href on the dropdown

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -34,7 +34,7 @@
                             <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
                         @endadmin
 
-                        <x-dropdown-item href="/posts/bookmarks" :active="request()->is('posts/bookmarked')">Bookmarks</x-dropdown-item>
+                        <x-dropdown-item href="/bookmarks" :active="request()->is('posts/bookmarked')">Bookmarks</x-dropdown-item>
 
                         <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
 
