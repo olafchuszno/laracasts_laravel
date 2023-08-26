cd7a2666083400de091cfda08167890acc147420
Olaf <olaf.chuszno@gmail.com>
2023-08-26 18:10:46 +0200

    Implement index action on Bookmark controller

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -12,13 +12,19 @@ class BookmarkController extends Controller
 
     public function index()
     {
-        ddd("Bookmarks index");
+        // return the view with the posts
+        return view('bookmarks.index', [
+            'posts' => Post::all()->
+                whereIn('id', Bookmark::select('post_id')->
+                where('user_id', auth()->id())->
+                get()->pluck('post_id')->toArray()
+            )
+        ]);
     }
 
     public function store(Post $post)
     {
         // associate the post' id with the user's id on the bookmarks table
-
         Bookmark::create([
             'user_id' => request()->user()->id,
             'post_id' => $post->id
