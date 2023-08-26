d3a9643cb60872d780cfcc3840008dfed416090d
Olaf <olaf.chuszno@gmail.com>
2023-08-26 13:47:45 +0200

    Establish hasMany relationships in user&post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -49,4 +49,9 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function bookmarks()
+    {
+        return $this->hasMany(Bookmark::class);
+    }
+
 }
