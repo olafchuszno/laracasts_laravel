126829e17728688ec477eb5efafe3100f0e21298
Olaf <olaf.chuszno@gmail.com>
2023-08-27 12:46:28 +0200

    Switch setting component in bookmarks.index

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-setting heading="Bookmarks">
+    <x-user-setting heading="Bookmarks">
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
