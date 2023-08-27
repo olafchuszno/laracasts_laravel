032f5e94681ada2538b2717e56b95d751e8c692b
Olaf <olaf.chuszno@gmail.com>
2023-08-27 12:15:34 +0200

    Implement a failure flash message

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -6,3 +6,12 @@
         <p>{{ session('success') }}</p>
     </div>
 @endif
+
+@if (session()->has('failure'))
+    <div x-data="{show:true}"
+        x-init="setTimeout(() => show = false, 4000)"
+        x-show="show"
+        class="fixed bg-red-500 text-white rounded-xl px-4 bottom-3 right-3 text-sm py-2">
+        <p>{{ session('failure') }}</p>
+    </div>
+@endif
