24186576ba8390ebdcb63c02f0bd7c1aadbc6bf3
Olaf <olaf.chuszno@gmail.com>
2023-08-27 17:45:57 +0200

    Pass authors to the view

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
@@ -35,7 +36,8 @@ class AdminPostController extends Controller
     public function edit(Post $post)
     {
         return view('admin.posts.edit', [
-            'post' => $post
+            'post' => $post,
+            'authors' => User::all()
         ]);
     }
 
