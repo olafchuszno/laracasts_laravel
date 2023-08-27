a4f987526555c6aeb92aa7a3b0d2f78730ddf42c
Olaf <olaf.chuszno@gmail.com>
2023-08-27 12:19:06 +0200

    Remove edit button in bookmarks.index

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -19,10 +19,6 @@
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a href="/admin/posts/{{ $post->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                             <form action="/admin/posts/{{ $post->id }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
