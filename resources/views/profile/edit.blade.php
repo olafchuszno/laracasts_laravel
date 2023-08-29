2a8adcf78da9ad0f929bc8e6ca286ed415672479
Olaf <olaf.chuszno@gmail.com>
2023-08-29 20:07:56 +0200

    Implement the profile.edit view

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-user-setting heading="Profile">
+
+        <div class="text-center mt-2 mb-10">
+            <heading class="text-blue-500 font-semibold border-2 rounded-xl border-blue-500 p-2"
+            >
+                Change your Profile information
+            </heading>
+        </div>
+
+        <form action="/profile" method="POST">
+            @csrf
+            @method('PATCH')
+
+            <x-form.input placeholder="Current name: {{ auth()->user()->username }}" name="name" />
+
+            <x-form.button>Update</x-form.button>
+
+        </form>
+
+    </x-user-setting>
+</x-layout>
