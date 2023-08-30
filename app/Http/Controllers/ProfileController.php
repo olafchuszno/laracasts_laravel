6c0f71366d39142883f01aa527d9227e23afe88d
Olaf <olaf.chuszno@gmail.com>
2023-08-30 18:49:08 +0200

    Implement update action in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -13,6 +14,24 @@ class ProfileController extends Controller
 
     public function update()
     {
-        dd('Update action in ProfileController');
+        // Find the user
+        $user = User::find(['id' => auth()->id()]);
+
+        // Check whether the user exists
+        if (count($user) != 1) {
+            back()->with('failure', 'Account not Found');
+        }
+
+        // Validate the input
+        $attributes = request()->validate([
+            'name' => ['required', 'max:255']
+        ]);
+
+        // Update the user's profile
+        $user[0]->update($attributes);
+
+        // redirect to the homepage
+        return back()->with('success', 'Your name was Updated');
+
     }
 }
