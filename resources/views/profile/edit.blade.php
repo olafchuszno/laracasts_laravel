bab2c47768961becf0e72e729db143eb918a4caa
Olaf <olaf.chuszno@gmail.com>
2023-09-01 21:08:21 +0200

    Show avatar (if set) next to the file input

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -14,8 +14,17 @@
 
             <x-form.input placeholder="Current name: {{ auth()->user()->username }}" label="New Name" name="name" />
 
-            <x-form.input type="file" name="avatar" required/>
             <!-- TODO show user's avatar next to the form.input -->
+            <div class="flex pt-2 pb-2 justify-between gap-5">
+
+                <x-form.input type="file" name="avatar" class="flex-auto" required />
+
+                @if (isset($user->avatar))
+                    <img src="{{ asset('storage/' . $user->avatar) }}" class="mr-1.5" width="100">
+                @endif
+            </div>
+            
+            
 
             <x-form.button>Update</x-form.button>
 
