4d5e0f0597980d05ca99be753d2c099d6e727891
Olaf <olaf.chuszno@gmail.com>
2023-09-02 18:48:37 +0200

    Add comments to RegisterControllers

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,7 +15,7 @@ class RegisterController extends Controller
 
     public function store()
     {
-        
+        // Store validate the input as attributes
         $attributes = request()->validate([
             'name' => ['required', 'max:255'],
             'avatar' => 'image',
@@ -39,15 +39,20 @@ class RegisterController extends Controller
             // Assign a random avatar 
             $path = "https://i.pravatar.cc/60?id={$randomNumber}";
 
+            // Store the generated avatar
             Storage::disk('public')->put("avatars/randomAvatarNumber{$randomNumber}.png", file_get_contents($path));
 
+            // Update the attributes variable
             $attributes['avatar'] = "avatars/randomAvatarNumber{$randomNumber}.png";
         }
 
+        // Create new account
         $user = User::create($attributes);
 
+        // Log the user in
         auth()->login($user);
 
+        // Redirect to the home page with a message
         return redirect('/')->with('success', 'Your account has been created');
     }
 }
